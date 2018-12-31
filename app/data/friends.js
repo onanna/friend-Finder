ce4fede122b568ef9d2ba0cf4729a4c7dcc43383
Bawo Nanna <nannaofe20@gmail.com>
2018-12-30 23:18:19 -0500

    commits completed offline

---
+++ app/data/friends.js
@@ -0,0 +1,39 @@
+var friends = [{
+    "name": "Erisdar Mysterious",
+    "photo": "https://pbs.twimg.com/profile_images/1054301623730741248/C2JF4yby_400x400.jpg",
+    "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+},
+{
+    "name": "Mistical Taytay",
+    "photo": "https://i.pinimg.com/280x280_RS/87/e6/12/87e6129a91c742f51d12a61aa45d687e.jpg",
+    "scores": [4, 2, 5, 1, 3, 2, 2, 1, 3, 2]
+},
+{
+    "name": "Skylar Rose",
+    "photo": "https://pbs.twimg.com/profile_images/1063216096012988416/th_jPtv9_400x400.jpg",
+    "scores": [5, 2, 2, 2, 4, 1, 3, 2, 5, 5]
+},
+{
+    "name": "Colonel Paul",
+    "photo": "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c4/Paul_W._Tibbets.JPG/220px-Paul_W._Tibbets.JPG",
+    "scores": [3, 3, 4, 2, 2, 1, 3, 2, 2, 3]
+},
+{
+    "name": "Edward Snowden",
+    "photo": "https://media.vanityfair.com/photos/5522970ffa699a350cfdf454/master/pass/a-edward-snowden-john-oliver.jpg",
+    "scores": [4, 3, 4, 1, 5, 2, 5, 3, 1, 4]
+},
+{
+    "name": "Hesso Nader",
+    "photo": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/Aatef_Jenyat.jpg/293px-Aatef_Jenyat.jpg",
+    "scores": [3, 3, 4, 1, 4, 3, 5, 3, 5, 4]
+},
+{
+    "name": "Sonneth Swift",
+    "photo": "https://i2.wp.com/img2-1.timeinc.net/people/i/2013/sandbox/news/131202/taylor-swift-600x450.jpg",
+    "scores": [4, 4, 2, 3, 2, 2, 3, 2, 4, 5]
+}
+
+]
+
+module.exports = friends;
